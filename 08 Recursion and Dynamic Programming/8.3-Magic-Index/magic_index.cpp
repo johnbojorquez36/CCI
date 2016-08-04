19cd91d42d8210d804a74371d5fae632f04f1b28
John Bojorquez <johnbojorquez36@gmail.com>
2016-08-03 20:24:50 -0400

    adding my solution

---
+++ 08 Recursion and Dynamic Programming/8.3-Magic-Index/magic_index.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <sstream>
+#include <algorithm>
+
+int magicIndex(std::vector<int> nums, int low, int high) {
+  if (high < low) {
+    return -1;
+  }
+  int mid = (high + low) / 2;
+  if (nums[mid] == mid) return mid;
+  else if (nums[mid] < mid) return magicIndex(nums, mid+1, high);
+  else return magicIndex(nums, low, mid-1);
+}
+  
+int magicIndex(std::vector<int> nums) {
+  return magicIndex(nums, 0, nums.size()-1);
+}
+
+std::vector<int> readVector(std::istream& in) {
+  std::string line;
+  std::vector<int> v;
+  getline(in, line);
+  std::stringstream ss(line);
+  int x;
+  while(ss >> x) v.push_back(x);
+  return v;
+}
+
+int main() {
+  std::cout << "Enter a list of numbers. They don't have to be sorted. "
+	    << "Don't worry, I'll sort them ;): " << std::endl;
+  std::vector<int> nums = readVector(std::cin);
+  std::sort(nums.begin(), nums.end());
+  std::cout << "The magic index is: " << magicIndex(nums) << std::endl;
+  return 0;
+}
